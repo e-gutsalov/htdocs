135e719e987b57fd43d81108574825b81475b0d3
gutsalov <e-gutsalov@mail.ru>
2019-11-21 01:22:38 +0300

    Исправление ошибок

--- components/Db.php
+++ components/Db.php
@@ -8,44 +8,9 @@
 
 namespace components;
 
-use PDO;
-use PDOException;
-
-class Db
+class DB
 {
-    /*private static $paramsPath,
-                   $params,
-                   $dsn,
-                   $db;
-
-    public function __construct()
-    {
-        $paramsPath = 'config/db_params.php';
-        $params = require $paramsPath;
-        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
-    }
-*/
-
     public static function getConnection()
     {
-        $paramsPath = 'config/db_params.php';
-        $params = require $paramsPath;
-        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
-        $options = [
-            PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ];
-
-        try
-        {
-            $db = new PDO( $dsn, $params['user'], $params['password'], $options );
-        }
-        catch ( PDOException $e )
-        {
-            echo 'Ошибка: ' . $e->getMessage();
-            exit();
-        }
-        return $db;
     }
 }
